bc5a828b4ede6c420b78bff3221da9ea32354fba
damduchai <damduchai02@gmail.com>
2025-04-12 12:20:13 +0700

    seed fake user data

---
+++ prisma/seed.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require('@prisma/client');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const users = require('./users.json');
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const user of users) {
+    await prisma.user.create({
+      data: user,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
